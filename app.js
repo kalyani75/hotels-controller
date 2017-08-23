d9284d588c8cb580933f83948f2c89c067253fdf
abalasu1 <abalasu1@in.ibm.com>
2017-08-23 23:53:13 +0530

    Latest Changes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var cors = require('cors')
 var async = require("async");
 
 var crypto = require('crypto');
+var redis = require('redis');
 
 const zipkin = require('zipkin');
 const wrapRequest = require('zipkin-instrumentation-request');
@@ -62,6 +63,24 @@ var dealqueryoptions = {
   host: process.env.DEALQUERYHOST || 'http://dealsquery-service',
   port: process.env.DEALQUERYPORT || 9013,
   path: '/hotels.com/api/v1.0/deals',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+
+var amenitiesqueryoptions = {
+  host: process.env.AMENITIESQUERYHOST || 'http://amenitiesquery-service',
+  port: process.env.AMENITIESQUERYPORT || 9014,
+  path: '/hotels.com/api/v1.0/amenities',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+
+var eventhandleroptions = {
+  host: process.env.EVENTHANDLERHOST || 'http://eventhandler-service',
+  port: process.env.EVENTHANDLERPORT || 9102,
+  path: '/hotels.com/api/v1.1/eventhandler',
   headers: {
     'Content-Type': 'application/json'
   }
@@ -128,6 +147,24 @@ var getsessionid = function() {
   return sha.digest('hex');
 }
 
+var gethotels = function(sessionid, latitude, longitude, radius, hotelscallback) {
+  var hotels = [];  
+  var reqoptions = {
+	  url: hotelqueryoptions.host + ':' + hotelqueryoptions.port + hotelqueryoptions.path + '/search/' + latitude + '/' + longitude,
+	  method: 'GET',
+	  headers: hotelqueryoptions.headers,  
+	  qs: {
+      'sessionid': sessionid,
+	    'radius': radius
+	  }
+  };
+
+  zipkinRequest(reqoptions, function (hotelserror, hotelsresponse, hotelsbody) {
+    hotelsJSON = JSON.parse(hotelsbody);
+    return hotelscallback(null, hotelsJSON);
+  });
+}
+
 var getalldeals = function(sessionid, dealcallback) {
   var deals = [];  
 	var reqoptions = {
@@ -140,8 +177,35 @@ var getalldeals = function(sessionid, dealcallback) {
   };
 
   zipkinRequest(reqoptions, function (dealserror, dealsresponse, dealsbody) {
-    dealsJSON = JSON.parse(dealsbody);
-    return dealcallback(null, dealsJSON);
+    if (typeof dealsbody !== 'undefined' && dealsbody) 
+    {    
+      dealsJSON = JSON.parse(dealsbody);
+      return dealcallback(null, dealsJSON);
+    }
+    else
+      return dealcallback(null, null);
+  });
+}
+
+var topamenities = function(sessionid, amenitiescallback) {
+  var amenities = [];  
+	var reqoptions = {
+	  url: amenitiesqueryoptions.host + ':' + amenitiesqueryoptions.port + amenitiesqueryoptions.path + '/topamenitiesbyhotel',
+	  method: 'GET',
+	  headers: amenitiesqueryoptions.headers,
+    qs: {
+      'sessionid': sessionid
+	  }    
+  };
+
+  zipkinRequest(reqoptions, function (amenitieserror, amenitiesresponse, amenitiesbody) {
+    if (typeof amenitiesbody !== 'undefined' && amenitiesbody) 
+    {
+      amenitiesJSON = JSON.parse(amenitiesbody);
+      return amenitiescallback(null, amenitiesJSON);
+    }
+    else
+      return amenitiescallback(null, null);
   });
 }
 
@@ -154,7 +218,7 @@ app.get('/hotels.com/controller/v1.0/hotels/search/:latitude/:longitude', cors()
   if (!longitude) controllerresponse.status(400).end();
 
   radius = parseInt(controllerrequest.query.radius);
-  if (!radius) pagelength = 5;
+  if (!radius) radius = 5;
 
   sessionid = getsessionid();
   var reqoptions = {
@@ -166,7 +230,7 @@ app.get('/hotels.com/controller/v1.0/hotels/search/:latitude/:longitude', cors()
 	    'radius': radius
 	  }
   };
-
+  
   zipkinRequest(reqoptions, function (hotelerror, hotelresponse, hotelbody) {
 	  if (!hotelerror && hotelresponse.statusCode == 200) {
       hotelsJSON = JSON.parse(hotelbody);
@@ -176,7 +240,8 @@ app.get('/hotels.com/controller/v1.0/hotels/search/:latitude/:longitude', cors()
       modifiedhotelsJSON.sessionid = sessionid;
 
       async.parallel({
-        alldeals: getalldeals.bind(null, sessionid)
+        alldeals: getalldeals.bind(null, sessionid),
+        topamenities: topamenities.bind(null, sessionid)
       }, function(fullhotelinfoerror, fullhotelinforesults) {
         var modifiedhotels = [];
         for (var i = 0; i < hotels.length; i ++)
@@ -186,7 +251,8 @@ app.get('/hotels.com/controller/v1.0/hotels/search/:latitude/:longitude', cors()
 
           hotelid = String(hotels[i].id)
           if (hotelid in fullhotelinforesults.alldeals) modifiedhotel.deals = _.propertyOf(fullhotelinforesults.alldeals)(hotelid);
-
+          if (hotelid in fullhotelinforesults.topamenities) modifiedhotel.topamenities = _.propertyOf(fullhotelinforesults.topamenities)(hotelid);
+          
           modifiedhotels.push(modifiedhotel);
         }
 
@@ -198,6 +264,52 @@ app.get('/hotels.com/controller/v1.0/hotels/search/:latitude/:longitude', cors()
 	    controllerresponse.status(400).end();
 	  }
   });
+});
+
+app.get('/hotels.com/controller/v1.1/hotels/search/:latitude/:longitude', cors(), function(controllerrequest, controllerresponse)
+{ 
+  latitude = parseFloat(controllerrequest.params.latitude);
+  if (!latitude) controllerresponse.status(400).end();
+
+  longitude = parseFloat(controllerrequest.params.longitude);
+  if (!longitude) controllerresponse.status(400).end();
+
+  radius = parseInt(controllerrequest.query.radius);
+  if (!radius) radius = 5;
+
+  sessionid = getsessionid();
+  async.parallel({
+    allhotels: gethotels.bind(null, sessionid, latitude, longitude, radius),
+    alldeals: getalldeals.bind(null, sessionid),
+    topamenities: topamenities.bind(null, sessionid)
+  }, function(fullhotelinfoerror, fullhotelinforesults) {
+    hotelsJSON = _.propertyOf(fullhotelinforesults.allhotels)('hotelsearch');
+
+    var hotels = [];
+    hotels = hotelsJSON;
+
+    var modifiedhotelsJSON = {};
+    modifiedhotelsJSON.sessionid = sessionid;    
+    
+    var finalhotelsearchresults = [];
+    for (var i = 0; i < hotels.length; i ++)
+    {
+      var hotel = {};
+      hotel = hotels[i];
+
+      hotelid = String(hotel.id)
+      if (typeof fullhotelinforesults.alldeals != undefined &&  fullhotelinforesults.alldeals && hotelid in fullhotelinforesults.alldeals) 
+        hotel.deals = _.propertyOf(fullhotelinforesults.alldeals)(hotelid);
+        
+      if (typeof fullhotelinforesults.topamenities != undefined &&  fullhotelinforesults.topamenities && hotelid in fullhotelinforesults.topamenities) 
+        hotel.topamenities = _.propertyOf(fullhotelinforesults.topamenities)(hotelid);
+          
+      finalhotelsearchresults.push(hotel);
+    }
+
+    modifiedhotelsJSON.hotelsearch = finalhotelsearchresults;
+    controllerresponse.json(modifiedhotelsJSON);
+  });  
 });
 
 // start server on the specified port and binding host
